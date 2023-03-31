8d4648d1d9ec859ac9571076b6d109035fddd1f0
asutarisucu <asutaricusu.s@gmail.com>
2023-03-31 11:20:56 +0900

    vertical-align: bottom;を追加

--- components/IframeFull.vue
+++ components/IframeFull.vue
@@ -2,7 +2,7 @@
   <iframe
     :src="data.sorce"
     class="w-100"
-    style="height: calc(100vh - 50px);z-index: 1;border: 0px;margin-top: 50px;"
+    style="height: calc(100vh - 50px);z-index: 1;border: 0px;margin-top: 50px;vertical-align: bottom;"
   ></iframe>
 </template>
 
